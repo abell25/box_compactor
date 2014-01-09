4c2d505b4414904e706079cc605e9e220a207bf3
abell25 <tb12485@yahoo.com>
2014-01-08 19:12:08 -0600

    best fit/worst fit varients

--- Layer.py
+++ Layer.py
@@ -1,6 +1,7 @@
 class Layer:
   def __init__(self, z, SLEIGH_LEN):
     self.packedPresents = [] # of form [[id px py pz] x y z]
+    self.packedPresentsBest = []
     self.z = z
     self.SLEIGH_LEN = SLEIGH_LEN  
 
@@ -74,6 +75,60 @@ class Layer:
 
     return [i, lz, packedPresents]
 
+  def packPresentInClosed(self, closedSpace, present):
+    id, px, py, pz = present
+    dimens = [px, py, pz]
+    configs = [[0, 1, 2], [0, 2, 1], [1, 0, 2], [2, 0, 1], [1, 2, 0], [2, 1, 0]]
+    cx, cmx, cy, cmy = closedSpace
+
+    for config in configs:
+      px, py, pz = dimens[config[0]], dimens[config[1]], dimens[config[2]], 
+        
+      if cmx-cx-1 > px and cmy-cy-1 > py:
+        packedPresent = [[id, px, py, pz], cx, cy, self.z]
+        shelf = [cx+px, cmx, cy, cmy]
+        return [True, packedPresent, shelf]
+
+    return [False, None, None]
+
+  
+  def packPresentInSpaces(self, present, closedSpaces):
+    used_closed_shelf = False
+    fits = []
+
+    for shelf in closedSpaces:
+      [used_shelf, packedPresent, closedShelf] = self.packPresentInClosed(shelf, present)
+      if used_shelf:
+        used_closed_shelf = True
+        fits.append([packedPresent, closedShelf, shelf])
+      
+    if used_closed_shelf:
+      # best fit -> shelf - present = sorted by unused space.
+      #fits.sort(key=lambda x: x[1][0]*x[1][1] - x[0][1]*x[0][2])
+      #packedPresent, closedShelf, shelf = fits[0] #best fit
+
+      # worst fit -> shelf - present = sorted by leftover space.
+      fits.sort(key=lambda x: x[1][0]*x[1][1] - x[0][1]*x[0][2], reverse=True)
+      packedPresent, closedShelf, shelf = fits[0] #worst fit
+      shelf[0] = closedShelf[0]
+
+      return [used_closed_shelf, packedPresent] 
+    
+    return [False, None]
+
+
+  def packPresentInSpaces3D(self, present, closedSpaces):
+    used_closed_shelf = False
+
+    for shelf in closedSpaces:
+      [used_closed_shelf, packedPresent, closedShelf] = self.packPresentInClosed3D(shelf, present)
+      if used_closed_shelf:
+        closedSpaces.append(closedShelf)
+        closedSpaces.remove(shelf)
+      return [used_closed_shelf, packedPresent] 
+    
+    return [False, None]
+  
   def packPresentInClosed3D(self, closedSpace, present):
     id, px, py, pz = present
     dimens = [px, py, pz]
@@ -98,66 +153,6 @@ class Layer:
         return [True, packedPresent, shelf]
 
     return [False, None, None]
-
-  def packPresentInClosed(self, closedSpace, present):
-    id, px, py, pz = present
-    dimens = [px, py, pz]
-    configs = [[0, 1, 2], [0, 2, 1], [1, 0, 2], [2, 0, 1], [1, 2, 0], [2, 1, 0]]
-    cx, cmx, cy, cmy = closedSpace
-
-    for config in configs:
-      px, py, pz = dimens[config[0]], dimens[config[1]], dimens[config[2]], 
-        
-      if cmx-cx-1 > px and cmy-cy-1 > py:
-        packedPresent = [[id, px, py, pz], cx, cy, self.z]
-        shelf = [cx+px, cmx, cy, cmy]
-        return [True, packedPresent, shelf]
-
-    return [False, None, None]
-
-  def packPresentInSpaces3D(self, present, closedSpaces):
-    used_closed_shelf = False
-
-    for shelf in closedSpaces:
-      [used_closed_shelf, packedPresent, closedShelf] = self.packPresentInClosed3D(shelf, present)
-      if used_closed_shelf:
-        closedSpaces.append(closedShelf)
-        closedSpaces.remove(shelf)
-      return [used_closed_shelf, packedPresent] 
-    
-    return [False, None]
-
-
-  def packPresentInClosed1(self, closedSpace, present):
-    id, px, py, pz = present
-    configs = [[0, 1, 2], [0, 2, 1], [1, 0, 2], [1, 2, 0], [1, 2, 0], [1, 0, 2]]
-    cx, cmx, cy, cmy = closedSpace
-      
-    if cmx-cx-1 > px and cmy-cy-1 > py:
-      packedPresent = [[id, px, py, pz], cx, cy, self.z]
-      shelf = [cx+px, cmx, cy, cmy]
-      return [True, packedPresent, shelf]
-      
-    px, py = py, px
-
-    if cmx-cx-1 > px and cmy-cy-1 > py:
-      packedPresent = [[id, px, py, pz], cx, cy, self.z]
-      shelf = [cx+px, cmx, cy, cmy]
-      return [True, packedPresent, shelf]
-
-    return [False, None, None]
-
-  def packPresentInSpaces(self, present, closedSpaces):
-    used_closed_shelf = False
-
-    for shelf in closedSpaces:
-      [used_closed_shelf, packedPresent, closedShelf] = self.packPresentInClosed(shelf, present)
-      if used_closed_shelf:
-        shelf[0] = closedShelf[0]
-      return [used_closed_shelf, packedPresent] 
-    
-    return [False, None]
-
 
   def getCoords(self):
     #converts the presents in each row to their coords
